6ac10830ed53c207d1afd3cb4f165cd725b7741f
Ivan Ivanov <me@ivanov.pro>
2024-12-20 21:20:33 +0200

    refactor colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,15 +2,17 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            bayern: {
-                'light': '#3B8FD9',
-                'medium': '#123C73',
-                'dark': '#123C73',
-                'white': '#EBEFF2',
-                'eggwhite': '#BACBD9'
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                bayern: {
+                    'light': '#3B8FD9',
+                    'medium': '#123C73',
+                    'dark': '#123C73',
+                    'white': '#EBEFF2',
+                    'eggwhite': '#BACBD9'
+                }
             }
         }
     },
