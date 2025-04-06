edbdc0769d55557207d775780fd5166647accd5f
Ivan Ivanov <me@ivanov.pro>
2025-04-06 11:11:32 +0300

    upgrade taiwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import icon from 'astro-icon';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://styria.it',
-    integrations: [icon(), tailwind(), react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [icon(), react()],
 });
